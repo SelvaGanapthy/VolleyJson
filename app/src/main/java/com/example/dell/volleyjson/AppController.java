353f6553341831ddca60346b38699d9b67f5c295
SelvaGanapthy <ganapathiselva16@gmail.com>
2017-12-01 00:21:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/volleyjson/AppController.java
@@ -0,0 +1,51 @@
+package com.example.dell.volleyjson;
+
+import android.app.Application;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Dell on 11/29/2017.
+ */
+
+public class AppController extends Application {
+    public static final String TAG = AppController.class.getSimpleName();
+    RequestQueue mRequestQueue;
+    static AppController mInstance;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null)
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> request, String tag) {
+        request.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getRequestQueue().add(request);
+    }
+
+    public <T> void addToRequestQueue(Request<T> request) {
+        request.setTag(TAG);
+        getRequestQueue().add(request);
+    }
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+    }
+}
